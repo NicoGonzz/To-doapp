f563112c83f8f9e19110a2046bab028a6a091a54
NicoGonzz <juannicolas28@live.com>
2023-11-22 17:06:22 -0500

    Task model creation and implementation update method

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -10,19 +11,67 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
 
-  tasks = signal([
-    ' Casa',
-    ' Cocina',
-    ' Baño',
-    'Dormitorio',
-    'Oficina',
-    'Jardín',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Arreglar la casa',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Arreglar la Cocina',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Arreglar el baño',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Arreglar el dormitorio',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Arreglar la oficina',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Arreglar el jardin',
+      completed: false
+    },
   ]);
 
   changeInput(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks,newTask]); //agregamos un nuevo elemento al final del array creando un nuevo estado
+    this.addTask(newTask);
+   // this.tasks.update((tasks) => [...tasks,newTask]); //agregamos un nuevo elemento al final del array creando un nuevo estado
+  }
+
+  addTask(title:string){
+    const newTask ={
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks,newTask]);
+  }
+
+  updateTask(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task,position)=> {
+        if (position ===index){
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
   }
 
   deleteTask(index: number){ //Tomamos la posicion de la tarea para eliminarla
